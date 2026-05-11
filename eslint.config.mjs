75040a8500e080394c5d739442d1adaa5d8c1da1
AchilleVivion <achille.vivion@outlook.fr>
2026-05-11 21:22:38 +0200

    implementing stricter linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import sonarjs from "eslint-plugin-sonarjs";
+import unicorn from "eslint-plugin-unicorn";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +15,27 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      sonarjs,
+      unicorn,
+    },
+    rules: {
+      "sonarjs/cognitive-complexity": ["error", 15],
+      "sonarjs/no-duplicate-string": "error",
+      "sonarjs/no-identical-functions": "error",
+      "unicorn/filename-case": ["error", { "cases": { "kebabCase": true, "pascalCase": true } }],
+      "unicorn/no-array-for-each": "error",
+      "unicorn/prevent-abbreviations": "off",
+      "unicorn/no-null": "off",
+      "max-lines-per-function": ["error", { 
+        "max": 30, 
+        "skipBlankLines": true, 
+        "skipComments": true,
+        "IIFEs": true,
+      }],
+    },
+  }
 ]);
 
 export default eslintConfig;
