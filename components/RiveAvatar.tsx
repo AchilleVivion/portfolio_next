605348fcd223213b26196b97928e8256a96f852e
AchilleVivion <achille.vivion@outlook.fr>
2026-05-11 17:18:42 +0200

    correct lint errors

--- components/RiveAvatar.tsx
+++ components/RiveAvatar.tsx
@@ -1,66 +1,64 @@
+// components/RiveCharacter.tsx
 'use client'
 
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
+import {
+  useRive,
+  useStateMachineInput,
+} from '@rive-app/react-webgl2'
 
 const avatarWidth = 500;
 const avatarHeight = 1000;
 
-const RiveComponent = dynamic(
-  () =>
-    import('@rive-app/react-webgl2').then((mod) => {
-      if (mod.RuntimeLoader) {
-        mod.RuntimeLoader.setWasmUrl('/rive.wasm')
-      }
+function RiveCanvas() {
 
-      return {
-        default() {
-          const { useRive, useStateMachineInput } = mod
-          const [isLoaded, setIsLoaded] = useState(false)
+  const { RiveComponent, rive } = useRive({
+    src: '/avatar.riv',
+    stateMachines: 'State Machine 1',
+    autoplay: true,
+  })
 
-          const { RiveComponent: RiveCanvas, rive } = useRive({
-            src: '/avatar.riv',
-            stateMachines: 'State Machine 1',
-            autoplay: true,
-            onLoad: () => setIsLoaded(true),
-          })
+  const waveInput = useStateMachineInput(rive, 'State Machine 1', 'wave')
 
-          const waveInput = useStateMachineInput(rive, 'State Machine 1', 'wave')
-          const scrollInput = useStateMachineInput(rive, 'State Machine 1', 'scrollDirection')
+  useEffect(() => {
+    if (waveInput) waveInput.fire()
+  }, [waveInput])
 
-          // Wave on loading
-          useEffect(() => {
-            if (waveInput) waveInput.fire()
-          }, [waveInput])
+  useEffect(() => {
+    return () => { rive?.cleanup() }
+  }, [rive])
 
-          // Cleanup
-          useEffect(() => {
-            return () => { rive?.cleanup() }
-          }, [rive])
+  return (
+    <div style={{ width: avatarWidth, height: avatarHeight }}>
+      <RiveComponent />
+    </div>
+  )
+}
 
-          return (
-            <div style={{ width: avatarWidth, height: avatarHeight }}>
-              <RiveCanvas />
-            </div>
-          )
-        },
-      }
-    }),
+const RiveComponentDynamic = dynamic(
+  async () => {
+    const mod = await import('@rive-app/react-webgl2')
+    if (mod.RuntimeLoader) {
+      mod.RuntimeLoader.setWasmUrl('/rive.wasm')
+    }
+    return { default: RiveCanvas }
+  },
   {
     ssr: false,
     loading: () => <div style={{ width: avatarWidth, height: avatarHeight }} />,
   }
 )
 
-export default function RiveAvatar() {
+export default function RiveCharacter() {
   const [isMounted, setIsMounted] = useState(false)
 
   useEffect(() => {
-    //eslint-disable-next-line react-hooks/set-state-in-effect
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setIsMounted(true)
   }, [])
 
   if (!isMounted) return <div style={{ width: avatarWidth, height: avatarHeight }} />
 
-  return <RiveComponent />
+  return <RiveComponentDynamic />
 }
