d408cc834979439bd67709e8757c97d4e23a9e4b
AchilleVivion <achille.vivion@outlook.fr>
2026-05-12 14:36:37 +0200

    fix lint error on proxy

--- proxy.ts
+++ proxy.ts
@@ -4,6 +4,5 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  // eslint-disable-next-line sonarjs/no-hardcoded-content
   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"], // NOSONAR
 };
